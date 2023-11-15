bcc6c7c5663e38f613230533c9f2a9b20aa9a16e
yuvrajdahal <srijald15@gmail.com>
2023-11-15 10:15:38 +0545

    health check

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -4,9 +4,8 @@ const connectDb = async () => {
   let url: string =
     (process.env.NODE_ENV === "production"
       ? process.env.MONGO_URL
-      : process.env.MONGO_DEV_URL) || "";
-  const conn = await mongoose.connect(url, {
-  });
+      : process.env.MONGO_URL) || "";
+  const conn = await mongoose.connect(url, {});
   console.log(`Mongoose Connected at: ${conn.connection.host}`);
 };
 
